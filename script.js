b3a99f08b04b7db87ee0b046809880ddda9edd1a
StephanieHarston <StephanieHarston@users.noreply.github.com>
2020-06-24 15:17:05 -0500

    adding starting files

---
+++ script.js
@@ -0,0 +1,35 @@
+var css = document.querySelector('h3');
+var color1 = document.querySelector('.color1');
+var color2 = document.querySelector('.color2');
+var body = document.getElementById('gradient');
+var randomize = document.querySelector('button');
+
+
+function setGradient () {   
+        body.style.background = 
+        "linear-gradient(to right,"
+        + color1.value
+        + ","
+        + color2.value
+        ")";
+        css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener('input', setGradient)
+color2.addEventListener('input', setGradient)
+
+
+// https://css-tricks.com/snippets/javascript/random-hex-color/ 
+function randomBackground () {
+    var randomColor1 = "#" + Math.floor(Math.random()*16777215).toString(16);
+    var randomColor2 = "#" + Math.floor(Math.random()*16777215).toString(16);
+    body.style.background = 
+    "linear-gradient(to right,"
+    + randomColor1
+    + ","
+    + randomColor2
+    + ")";
+    css.textContent = body.style.background + ";";
+}
+
+randomize.addEventListener("click",randomBackground);
